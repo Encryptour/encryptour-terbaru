68ae27b35783f5fb4c4a4183ecb97ceac6775900
KuroiiDev <maitsamkadzim26@gmail.com>
2025-06-16 06:54:14 +0700

    Mahasiswa models

--- app/Models/mahasiswa.php
+++ app/Models/mahasiswa.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class mahasiswa extends Model
 {
-    /** @use HasFactory<\Database\Factories\MahasiswaFactory> */
     use HasFactory;
+    protected $primaryKey = 'nim';
+    public $incrementing = false;
+    protected $keyType = 'string';
+    protected $table = 'mahasiswas';
+    protected $fillable = [
+        'nim', 'email_adress', 'nama_lengkap', 'nama_panggilan', 'agama', 'asal', 'ttl',
+        'alamat_rumah', 'alamat_kos', 'hobi', 'quotes', 'tempat_makan_fav',
+        'no_wa', 'user_ig', 'nama_wali', 'no_telp_wali', 'formal_picture', 'non_formal_picture', 'mdpl'
+    ];
 }
--- database/migrations/2025_06_15_234852_create_mahasiswas_table.php
+++ database/migrations/2025_06_15_234852_create_mahasiswas_table.php
@@ -12,7 +12,25 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('mahasiswas', function (Blueprint $table) {
-            $table->id();
+            $table->unsignedBigInteger('nim')->primary();
+            $table->string('email_adress')->unique();
+            $table->string('nama_lengkap');
+            $table->string('nama_panggilan');
+            $table->string('agama');
+            $table->string('asal');
+            $table->string('ttl');
+            $table->text('alamat_rumah');
+            $table->string('alamat_kos');
+            $table->string('hobi')->nullable();
+            $table->text('quotes')->nullable();
+            $table->string('tempat_makan_fav')->nullable();
+            $table->string('no_wa');
+            $table->string('user_ig')->nullable();
+            $table->string('nama_wali');
+            $table->string('no_telp_wali');
+            $table->string('formal_picture')->nullable();
+            $table->string('non_formal_picture')->nullable();
+            $table->integer('mdpl')->nullable();
             $table->timestamps();
         });
     }

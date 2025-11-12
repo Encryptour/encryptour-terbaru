1958861275c60ca92dc322b3b9856e68f7b40b11
KuroiiDev <maitsamkadzim26@gmail.com>
2025-11-12 23:19:13 +0700

    Inject

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,10 @@
 import Alpine from "alpinejs";
 import { track } from "@vercel/analytics";
+import { inject } from "@vercel/analytics";
 
 Alpine.start();
 
 track("pageview", {
   url: window.location.href,
 });
+inject();
